c375798c4f52b88b37062770909682f96ef5118b
Tyler Fisher <tfisher@politico.com>
2018-02-26 13:27:33 -0500

    trying some list filters on votes

--- vote/admin.py
+++ vote/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from vote.models import Delegates, ElectoralVotes, Votes
 
+
+class VotesAdmin(admin.ModelAdmin):
+    list_filter = (
+        'candidate_election__election__division__level',
+        'candidate_election__election',
+    )
+
+
 admin.site.register(Delegates)
 admin.site.register(ElectoralVotes)
-admin.site.register(Votes)
+admin.site.register(Votes, VotesAdmin)
