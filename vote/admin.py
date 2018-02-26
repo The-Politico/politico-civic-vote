28a70f6c5f5e1d18dbab61664f30f9a4d9c7dab9
Tyler Fisher <tfisher@politico.com>
2018-02-26 13:36:41 -0500

    nvm, doesn't work at all

--- vote/admin.py
+++ vote/admin.py
@@ -1,13 +1,6 @@
 from django.contrib import admin
 from vote.models import Delegates, ElectoralVotes, Votes
 
-
-class VotesAdmin(admin.ModelAdmin):
-    list_filter = (
-        'candidate_election__election__division__level',
-    )
-
-
 admin.site.register(Delegates)
 admin.site.register(ElectoralVotes)
-admin.site.register(Votes, VotesAdmin)
+admin.site.register(Votes)
