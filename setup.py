ddfebcb15ecbbda9894c7319aac7a8500022494b
Tyler Fisher <tfisher@politico.com>
2018-06-20 17:17:23 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='politico-civic-vote',
-    version='0.1.0',
+    version='0.1.1',
     description='',
     url='https://github.com/The-Politico/politico-civic-vote',
     author='POLITICO interactive news',
