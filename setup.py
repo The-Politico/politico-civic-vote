7b6c5b2146a3ad9675b79e9f10ef3a109cb03d60
Tyler Fisher <tfisher@politico.com>
2018-08-22 11:20:48 -0400

    update election

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='politico-civic-vote',
-    version='0.1.3',
+    version='0.1.4',
     description='',
     url='https://github.com/The-Politico/politico-civic-vote',
     author='POLITICO interactive news',
