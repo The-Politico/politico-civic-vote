4eb893956182bff1a0859cbdd722959972806690
Tyler Fisher <tfisher@politico.com>
2018-11-15 13:52:42 -0500

    bootstrap command

---
+++ vote/management/commands/bootstrap_vote.py
@@ -0,0 +1,15 @@
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Bootstrap development."
+
+    def handle(self, *args, **options):
+        call_command("bootstrap_geography")
+        call_command("bootstrap_jurisdictions")
+        call_command("bootstrap_fed")
+        call_command("bootstrap_offices")
+        call_command("bootstrap_parties")
+        call_command("bootstrap_election_events")
+        call_command("schedule_elections")
