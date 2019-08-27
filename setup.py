07641b8cedae2da8bdcedd11535cef6b1635c438
Allan James Vestal <allanjamesvestal@gmail.com>
2019-08-27 01:06:56 -0400

    Modify setup.py: blacken, change to binary version of psycopg2 dependency, use variable for version.

--- setup.py
+++ setup.py
@@ -1,10 +1,34 @@
-from setuptools import find_packages, setup
+# Imports from python.
+import os
+from setuptools import find_packages
+from setuptools import setup
+
+
+# Imports from election.
+from vote import __version__
+
+
+REPO_URL = "https://github.com/The-Politico/politico-civic-vote/"
+
+PYPI_VERSION = ".".join(str(v) for v in __version__)
+
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
+    README = readme.read()
+
 
 setup(
     name="politico-civic-vote",
-    version="0.2.0",
-    description="",
-    url="https://github.com/The-Politico/politico-civic-vote",
+    version=PYPI_VERSION,
+    packages=find_packages(exclude=["docs", "tests", "example"]),
+    description=(
+        "Manage votes, electoral votes and delegate counts, the POLITICO way."
+    ),
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url=REPO_URL,
+    download_url="{repo_url}archive/{version}.tar.gz".format(
+        **{"repo_url": REPO_URL, "version": PYPI_VERSION}
+    ),
     author="POLITICO interactive news",
     author_email="interactives@politico.com",
     license="MIT",
@@ -20,7 +44,6 @@ setup(
         "Topic :: Internet :: WWW/HTTP",
     ],
     keywords="",
-    packages=find_packages(exclude=["docs", "tests"]),
     install_requires=[
         "django",
         "djangorestframework",
