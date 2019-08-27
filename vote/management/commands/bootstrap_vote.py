a0a9c0ab7dedc0cab7e1d2a47c713708b9e61214
Allan James Vestal <allanjamesvestal@gmail.com>
2019-08-27 00:55:21 -0400

    Blacken management command files and standardize their imports.

--- vote/management/commands/bootstrap_vote.py
+++ vote/management/commands/bootstrap_vote.py
@@ -1,5 +1,6 @@
-from django.core.management import call_command
+# Imports from Django.
 from django.core.management.base import BaseCommand
+from django.core.management import call_command
 
 
 class Command(BaseCommand):
